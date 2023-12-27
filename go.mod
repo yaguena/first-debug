ad8d7030227ec69945daf0ba3cfe47ee9999b386
Gabriel Aguena <yoshikichi@gmail.com>
2023-12-26 22:51:36 -0300

    config vscode debug

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gaguena/first-debug
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	now := "Gabriel Aguena"
+
+	fmt.Println("Hello, %v", now)
+}
